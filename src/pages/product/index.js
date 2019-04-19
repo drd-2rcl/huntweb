1f111cf62a22597a9be4ca00ca400d9f1e48379d
drd-2rcl <drd.rclrd@gmail.com>
2019-04-19 12:24:16 -0300

    call api for detail data product id

--- src/pages/product/index.js
+++ src/pages/product/index.js
@@ -1,9 +1,35 @@
 import React, { Component } from 'react'
+import Axios from 'axios';
 
+import './styles.css'
+ 
 class Product extends Component {
+  state = {
+    product: {}
+  }
+  
+  async componentDidMount() {
+    const { id } = this.props.match.params;
+
+    const response = await Axios.get(
+      `https://rocketseat-node.herokuapp.com/api/products/${id}`
+    ); 
+
+    this.setState({ product: response.data });
+  }
+
   render() {
+    const { product } = this.state;
+
     return (
-      <h1>Product</h1>
+      <div className="product-info">
+        <h1>{product.title}</h1>
+        <p>{product.description}</p>
+
+        <p>
+          URL: <a href={product.url} target="_blank" >{product.url}</a>
+        </p>
+      </div>
     )
   }
 }
