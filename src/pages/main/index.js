c9815de4f71142ca2c356ca4ef5c3e5d134aef4a
drd-2rcl <drd.rclrd@gmail.com>
2019-04-19 11:32:23 -0300

    add buttons for prev and next page data api

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -6,6 +6,8 @@ import './styles.css';
 export default class Main extends Component {
   state = {
     products: [],
+    productInfo: {},
+    page: 1,
   }
 
   componentDidMount() {
@@ -13,20 +15,39 @@ export default class Main extends Component {
   }
 
   // quando utilizamos métodos que pertencem ao react utilizamos o modelo normal de named function, mas quando criamos uma função nossa precisa ser com Arrow Function para que a nossão função consiga enchergar o escopo da variável this, para que assim a função possa se referenciar a classe e acessar outros métodos e variáveis da classe;
-
   // async e await facilita a maneira de lidar com promises dentro do javascript (menos verboza)
   
-  loadProducts = async () => {
-    const response = await Axios.get('https://rocketseat-node.herokuapp.com/api/products');
-    // console.log(response.data.docs);
-    this.setState({ products: response.data.docs })
+  loadProducts = async (page = 1) => {
+    const response = await Axios.get(`https://rocketseat-node.herokuapp.com/api/products?page=${page}`); 
+    
+    const { docs, ...productInfo } = response.data;
+    
+    this.setState({ products: docs, productInfo, page });
+  };
+
+  prevPage = () => {
+    const { page, productInfo } = this.state;
+
+    if ( page === 1) return;
+
+    const pageNumber = page - 1;
+
+    this.loadProducts(pageNumber);
+
   }
 
-  prevPage = () => {}
-  nextPage = () => {}
+  nextPage = () => {
+    const { page, productInfo } = this.state;
+
+    if (page === productInfo.pages) return;
+
+    const pageNumber = page + 1;
+
+    this.loadProducts(pageNumber);
+  }
 
   render() {
-    const { products } = this.state;
+    const { products, page, productInfo } = this.state;
     return (
       <div className="product-list">
         {products.map(product => (
@@ -37,8 +58,8 @@ export default class Main extends Component {
           </article>
         ))}
         <div className="actions" >
-          <button onClick={this.prevPage} >Anterior</button>
-          <button onClick={this.nextPage} >Próximo</button>
+          <button disabled={page === 1} onClick={this.prevPage} >Anterior</button>
+          <button disabled={page === productInfo.pages} onClick={this.nextPage} >Próximo</button>
         </div>
       </div>
     )
